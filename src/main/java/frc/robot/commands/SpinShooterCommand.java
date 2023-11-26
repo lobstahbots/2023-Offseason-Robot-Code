31245889cae811e50f730c818f173e0d653ff24f
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2023-11-26 17:25:49 -0500

    Make SpinShooterCommand spin the shooter

--- src/main/java/frc/robot/commands/SpinShooterCommand.java
+++ src/main/java/frc/robot/commands/SpinShooterCommand.java
@@ -4,25 +4,42 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
 
-public class SpinShooterCommand extends CommandBase {
-  /** Creates a new SpinShooterCommand. */
-  public SpinShooterCommand() {
+public class SpinShooterCommand extends Command {
+  private final Shooter shooter;
+  private final double mainSpeed;
+  private final double auxiliarySpeed;
+  /**
+   * Command to spin the shooter's ({@link Shooter}) main and auxiliary motor.
+   * @param shooter The shooter subsystem.
+   * @param mainSpeed The speed for the main motor.
+   * @param auxiliarySpeed The speed for the auxiliary motor.
+   */
+  public SpinShooterCommand(Shooter shooter, double mainSpeed, double auxiliarySpeed) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.shooter = shooter;
+    this.mainSpeed = mainSpeed;
+    this.auxiliarySpeed = auxiliarySpeed;
+    addRequirements(shooter);
   }
-
-  // Called when the command is initially scheduled.
+  /**
+   * Execute the command; called every time the scheduler is run. Sets the speed of both motors to their respective speeds.
+   */
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
+  public void execute() {
+    shooter.mainDrive(mainSpeed);
+    shooter.auxiliaryDrive(auxiliarySpeed);
+  }
+  /**
+   * End the command; called when the command terminates. Stops both motors.
+   */
   @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    shooter.stopAuxiliary();
+    shooter.stopMain();
+  }
 
   // Returns true when the command should end.
   @Override
