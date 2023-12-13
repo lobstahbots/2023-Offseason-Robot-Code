8b4570c03979f7005c846833809ed3c51c66f27e
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2023-12-13 09:38:33 -0500

    Add SpinIntakeRollerCommand

---
+++ src/main/java/frc/robot/commands/SpinIntakeRollerCommand.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+
+public class SpinIntakeRollerCommand extends Command {
+  private final Intake intake;
+  private final double speed;
+  /**
+   * Command to spin the intake's ({@link Intake}) roller.
+   * @param intake The intake subsystem.
+   * @param speed The speed for the roller.
+   */
+  public SpinIntakeRollerCommand(Intake intake, double speed) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.intake = intake;
+    this.speed = speed;
+    addRequirements(intake);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    intake.setRollerSpeed(speed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intake.stopRoller();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
